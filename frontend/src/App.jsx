d5b4cc81d62779377d4cadbdf085ae23325c2989
Adam Akbar <graha.asa@gmail.com>
2019-10-03 16:16:25 +0700

    Fix navbar location

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
-
+import Navbar from './components/layouts/navbar/NavBar';
 import LoginPage from './pages/auth/Login';
 import RegisterPage from './pages/auth/Register';
 import HomePage from './pages/Home';
@@ -9,14 +9,16 @@ const token = localStorage.token;
 class App extends Component {
   render() {
     return (
-      <Router>
-        <Switch>
-        
-          <Route path="/login" component={LoginPage} />
-          <Route path="/" component={HomePage} exact />
-          <Route path="/register" component={RegisterPage} />
-        </Switch>
-      </Router>
+        <Router>
+          <Navbar />
+
+          <Switch>
+
+            <Route path="/login" component={LoginPage} />
+            <Route path="/" component={HomePage} exact />
+            <Route path="/register" component={RegisterPage} />
+          </Switch>
+        </Router>
     )
   }
 }
