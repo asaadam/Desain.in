a40e77247640a7920d0823b44f43027730f96575
unknown <graha.asa@gmail.com>
2019-10-04 22:08:16 +0700

    fix Navbar Routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
-import React, { Component, Fragment } from 'react';
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
-
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layouts/navbar/NavBar';
 import LoginPage from './pages/auth/Login';
 import RegisterPage from './pages/auth/Register';
 import HomePage from './pages/Home';
@@ -8,14 +8,22 @@ import ChatPage from './pages/chat/ChatPage';
 import CreateContestPage from './pages/contest/CreateContest';
 import ProfilePage from './pages/profile/UpdateProfile';
 
+
+
+
+
 class App extends Component {
   render() {
     return (
       <Router>
+
+        <Switch>
+        <Route path="/login" component={LoginPage} />
+          <Route path="/register" component={RegisterPage} />
+        </Switch>
+        <Navbar/>
         <Switch>
           <Route path="/" component={HomePage} exact />
-          <Route path="/login" component={LoginPage} />
-          <Route path="/register" component={RegisterPage} />
           <Route path="/chat" component={ChatPage} />
           <Route path="/create-contest" component={CreateContestPage} />
           <Route path="/profile" component={ProfilePage} />
@@ -24,5 +32,7 @@ class App extends Component {
     )
   }
 }
+
+
 
 export default App;
